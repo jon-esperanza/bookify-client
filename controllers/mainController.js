2cfca4e73b4ec38c972efe63d2989af67bdef4ff
jon esperanza <jonathanesperanza67@gmail.com>
2022-07-12 22:10:13 -0400

    setup MVC structure for routing to controllers

---
+++ controllers/mainController.js
@@ -0,0 +1,19 @@
+exports.index = (req, res, next) => {
+    const renderObj = {
+        isAuthenticated: req.oidc.isAuthenticated(), 
+        user: req.oidc.user
+    }
+    if (req.oidc.isAuthenticated()) {
+        res.render('./user/dashboard', renderObj);
+    } else {
+        res.render('./index', renderObj);
+    }
+}
+exports.signup = (req, res, next) => {
+    res.oidc.login({
+        returnTo: '/',
+        authorizationParams: {
+            screen_hint: 'signup',
+        },
+    });
+}
