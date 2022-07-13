bbb36748162958c156595ffc3112122a9663ce79
jon esperanza <jonathanesperanza67@gmail.com>
2022-07-12 21:50:57 -0400

    configuring new dependancies for app

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-import express from 'express';
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
 
 // create app
 const app = express();
@@ -6,16 +8,40 @@ const app = express();
 // configure app
 let port = 3000;
 let host = 'localhost';
+app.set('view engine', 'ejs');
 
-// middlewares
+// mount middlewares
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
 
 // routes
 app.get('/', (req, res, next) => {
-    res.send('hi this is still working just making a change.');
+    res.render('./index');
+});
+
+// handle undefined routes
+app.use((req, res, next) => {
+    let err = new Error('The server cannot locate ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
+// handle errors -> redirect to error page
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if(!err.status) {
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+    res.status(err.status);
+    res.render('error', {
+        error: err
+    });
 });
 
 // start server
-app.listen(port, host, ()=>{
+app.listen(port, host, () => {
     console.log('Server is running on port', port);
 });
